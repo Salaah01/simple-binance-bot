0555d04be94c7f442263bafc6f0b1c630928f156
Salaah Amin <salaah_amin@hotmail.co.uk>
2021-05-10 23:35:46 +0100

    written a bollinger test in the if main == name, added some test data to work with.

--- bot/strategies/bollinger.py
+++ bot/strategies/bollinger.py
@@ -2,7 +2,10 @@
 
 import numpy as np
 import pandas as pd
-from .strategy_base import Strategy
+try:
+    from .strategy_base import Strategy
+except ImportError:
+    from strategy_base import Strategy
 
 
 class Bollinger(Strategy):
@@ -19,12 +22,19 @@ class Bollinger(Strategy):
 
         # Edgecase
         if len(npCloses) < period + 1:
-            return {'results': {'low': '', 'high': ''}, 'decision': 0}
+            return {
+                'results': {
+                    'Bollinger Low': '',
+                    'Bollinger High': '',
+                    'Bollinger Decision': 0
+                },
+                'decision': 0
+            }
 
         # Convert the closing prices into a Pandas dataframe and add columns
         # with certain statistical information that will help calculate the
         # Bollinger value.
-        npCloses = npCloses[-period + 1:]
+        npCloses = npCloses[-period - 1:]
         df = pd.DataFrame(npCloses, columns=['Close'])
 
         df['SMA'] = df['Close'].rolling(window=period).mean()
@@ -55,3 +65,24 @@ class Bollinger(Strategy):
             },
             'decision': decision
         }
+
+
+if __name__ == '__main__':
+    import os
+    print(os.path.abspath('.'))
+    data = np.genfromtxt(os.path.join('test_data', 'closing_prices.csv'))
+
+    phasedData = []
+    coinsOwned = False
+    for d in data:
+        phasedData.append(float(d))
+        results = Bollinger().apply_indicator(
+            np.array(phasedData),
+            {'period': 20},
+            coinsOwned
+        )
+        print(f"{results['results']['Bollinger Low']}|{results['results']['Bollinger High']}|{results['results']['Bollinger Decision']}")
+        if results['decision'] == 1:
+            coinsOwned = True
+        elif results['decision'] == -1:
+            coinsOwned = False
