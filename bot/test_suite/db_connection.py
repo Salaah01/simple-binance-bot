7fecdddde6600ad3481cba04e239823a2ae9abbe
Salaah Amin <salaah_amin@hotmail.co.uk>
2021-05-21 17:35:24 +0100

    script to dump all coins.

--- bot/test_suite/db_connection.py
+++ bot/test_suite/db_connection.py
@@ -1,16 +1,17 @@
 from configparser import ConfigParser
 import psycopg2
 
-def connection(filename:str='database.ini', section:str='postgresql') -> dict:
+
+def connection(filename: str = 'database.ini', section: str = 'postgresql'):
     """Fetches the database connection parameters by parsing the connection
     .ini file.
 
     Args:
         filename - (str) .ini filename.
         section - (str) Section relating to the database config in the .ini.
-    
+
     Returns:
-        dict - Database connection paramaters.
+        Database connection.
     """
     parser = ConfigParser()
     parser.read(filename)
@@ -20,10 +21,10 @@ def connection(filename:str='database.ini', section:str='postgresql') -> dict:
         params = parser.items(section)
         for param in params:
             db[param[0]] = param[1]
-    
+
     else:
         raise Exception(f'Section {section} not found in {filename}.')
 
     return psycopg2.connect(
-        f"dbname={db['database']} user={db['user']} password={db['password']} host={db['host']} port={db['port']}"
+        f"dbname={db['database']} user={db['user']} password={db['password']} host={db['host']} port={db['port']}"  # noqa: E501
     )
---
+++ scripts/dump_all.py
@@ -0,0 +1,47 @@
+"""Sells all the coins."""
+
+import sys
+import os
+import json
+import traceback
+from binance.enums import SIDE_SELL
+ROOT = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+sys.path.append(ROOT)
+from bot.send_order_signal import SendOrderSignal  # noqa: E402
+
+TEST_MODE = True
+
+
+# Load variables from config file.
+with open(os.path.join(ROOT, 'bot', 'config.json')) as jsonFile:
+    CONFIG = json.load(jsonFile)
+
+tradeSymbols = CONFIG['trade_symbols']
+tradeCurrencies = CONFIG['trade_currencies']
+
+signal = SendOrderSignal()
+for tradeSymbol in tradeSymbols:
+    try:
+        # Remove the currency from the coin name.
+        symbol = tradeSymbol
+        for tradeCurrency in tradeCurrencies:
+            symbol = symbol.rstrip(tradeCurrency)
+
+        # Check the asset balance
+        balance = signal.asset_balance(symbol)
+        signal.respect_request_limit()
+
+        # If there is no balance, move onto the next coin.
+        if not float(balance):
+            continue
+
+        # Sell the assets.
+        res = signal.send_signal(SIDE_SELL, tradeSymbol, balance, TEST_MODE)
+        signal.respect_request_limit()
+        if res['success']:
+            print(f'\033[92mSold {tradeSymbol}\033[0m')
+        else:
+            print(F'\033[0mFAILED TO SELL {tradeSymbol}\033[0m')
+    except Exception:
+        print(traceback.format_exc())
+        print(f'\033[91m FAILED TO SELL {tradeSymbol}.\033[91m')
