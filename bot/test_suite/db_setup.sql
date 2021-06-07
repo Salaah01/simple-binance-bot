76dbc53059a9e0b6fdb28bb0ee3c2f4e8fe60d96
Samiul Amin <samiulamin@hotmail.co.uk>
2021-06-07 11:19:57 +0100

    Changed the interval from 1m to 5m

--- bot/test_suite/db_setup.sql
+++ bot/test_suite/db_setup.sql
@@ -1,7 +1,7 @@
 -- Running the following SQL to create the tables if the database has not
 -- been set up yet.
 CREATE TABLE symbols (symbol VARCHAR(10) PRIMARY KEY);
-CREATE TABLE prices (
+CREATE TABLE prices_1m (
   id SERIAL PRIMARY KEY,
   open_time TIMESTAMP,
   symbol VARCHAR(10),
@@ -19,3 +19,21 @@ CREATE TABLE prices (
   UNIQUE (open_time, symbol)
 );
 CREATE TABLE loaded_files(file VARCHAR(100) PRIMARY KEY UNIQUE);
+
+CREATE TABLE prices_5m (
+  id SERIAL PRIMARY KEY,
+  open_time TIMESTAMP,
+  symbol VARCHAR(10),
+  open_price FLOAT,
+  high_price FLOAT,
+  low_price FLOAT,
+  close_price FLOAT,
+  volume FLOAT,
+  close_time TIMESTAMP,
+  quote_asset_volume FLOAT,
+  no_traders INTEGER,
+  taker_buy_base_asset_vol FLOAT,
+  taker_buy_quote_asset_vol FLOAT,
+  CONSTRAINT fk_symbol FOREIGN KEY(symbol) REFERENCES symbols(symbol),
+  UNIQUE (open_time, symbol)
+);
